00481a3447547116be4379f7c76988e227ba5057
mabarbar <mateusz.barzenc00@gmail.com>
2022-09-03 15:56:22 +0200

    started working on colors of the notes

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ const errorMessage = document.querySelector(".error");
 
 let cardID = 0;
 
+let colorWheel = document.querySelector(".color");
+let selectedValue;
+
 const showSettings = () => {
   noteMenu.classList.toggle("active");
   resetSettings();
 };
-
-// const deleteNote = () => {console.log("si");};
 
 const deleteAllNotes = () => {
   notesSection.textContent = "";
@@ -27,37 +28,35 @@ const addNote = () => {
     noteText.value !== "" &&
     noteCategory.options[noteCategory.selectedIndex].value !== "0"
   ) {
-    console.log(noteCategory.options[noteCategory.selectedIndex].value);
     saveNote();
   } else {
     errorMessage.textContent = "Please fill all text areas.";
     errorMessage.style.color = "red";
-    console.log(noteCategory.options[noteCategory.selectedIndex].value);
   }
 };
 
 const saveNote = () => {
   const newNote = document.createElement("div");
   newNote.setAttribute("ID", cardID);
+  newNote.classList.add("note");
   newNote.innerHTML = `
-  <div class="note">
   <div class="noteHeader">
-    <h2 class="noteTitle">${
-      noteCategory.options[noteCategory.selectedIndex].textContent
-    }</h2>
-    <button class="deleteButton">
-      <i class="fa-regular fa-circle-xmark"></i>
-    </button>
+  <h2 class="noteTitle">${
+    noteCategory.options[noteCategory.selectedIndex].textContent
+  }</h2>
+  <button class="deleteButton">
+  <i class="fa-regular fa-circle-xmark"></i>
+  </button>
   </div>
   <p class="noteContent">
   ${noteText.value}
   </p>
-</div>`;
+  </div>`;
 
   notesSection.appendChild(newNote);
+  checkColor(newNote);
   showSettings();
   resetSettings();
-
   cardID++;
 };
 
@@ -69,12 +68,42 @@ const resetSettings = () => {
 };
 
 const deleteNote = (e) => {
-  console.log(e.target);
   if (e.target.matches(".deleteButton")) {
     const noteToDelete = e.target.closest(".note");
     noteToDelete.remove();
   }
 };
+
+const selectValue = () => {
+  selectedValue = noteCategory.options[noteCategory.selectedIndex].textContent;
+};
+
+const checkColor = (note) => {
+  selectValue();
+  switch (selectedValue) {
+    case "Notatka":
+      note.style.background = "hsl(60, 100%, 50%)";
+      note.style.box_shadow = "hsl(80, 50%, 50%)";
+      break;
+    case "Work":
+      note.style.background = "hsl(195, 53%, 79%)";
+      break;
+    case "Home":
+      note.style.background = "hsl(9, 100%, 64%)";
+      break;
+    case "Shopping":
+      note.style.background = "hsl(54, 77%, 75%)";
+      break;
+    case "Hobby":
+      note.style.background = "hsl(84, 100%, 59%)";
+      break;
+    case "Other":
+      note.style.background = "hsl(0, 0%, 100%";
+      break;
+  }
+};
+
+selectValue();
 
 btnAdd.addEventListener("click", showSettings);
 btnCancel.addEventListener("click", showSettings);
